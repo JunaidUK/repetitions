ad0644aacf99ffcae9199e85fcb21a1652ecf80e
Junaid Siddiqui <junaid.wim@gmail.com>
2018-10-29 14:14:05 -0400

    Added react components and google maps api

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Route, IndexRoute, Router, browserHistory } from 'react-router'
+import Homepage from '../containers/Homepage'
 
 export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+  return (
+    <div>
+      <Router history={browserHistory}>
+        <Route path='/interfaces' component={Homepage} />
+        <Route path='/' component={Homepage} />
+      </Router>
+    </div>
+  )
 }
 
 export default App

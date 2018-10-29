6ad77856df56406f474f657fa0ef61dd52c2de3e
Junaid Siddiqui <junaid.wim@gmail.com>
2018-10-29 14:15:50 -0400

    Built out react router and added small components

---
+++ app/controllers/interfaces_controller.rb
@@ -0,0 +1,5 @@
+class InterfacesController < ApplicationController
+  def index
+    
+  end
+end
---
+++ app/javascript/react/containers/Homepage.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import GoogleMapReact from 'google-map-react'
+
+class Homepage extends Component {
+  constructor(props) {
+      super(props)
+      this.state = {
+        center:{
+        lat: 26.092891,
+        lng: 85.948970
+        },
+        zoom: 11,
+        apikey:""
+      }
+    }
+    render(){
+      return(
+        <div>
+          <h1>This area will have more react components</h1>
+          <div style={{ height: '50vh', width: '100%' }}>
+           <GoogleMapReact
+             bootstrapURLKeys={{key: this.state.apikey}}
+             defaultCenter={this.state.center}
+             defaultZoom={this.state.zoom}
+           >
+           </GoogleMapReact>
+          </div>
+        </div>)
+    }
+}
+export default Homepage;
